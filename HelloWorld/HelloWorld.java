ce313de6e461142f521635ace78ed88a8525b17f
baemacs <66023849+baemacs@users.noreply.github.com>
2022-04-25 10:58:48 +0800

    Added my name.

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello again World!");
         System.out.println("I was here - Edmark");
+        System.out.println("Mac was here <3");
     }
 }
