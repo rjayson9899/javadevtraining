7296689f19a403f0f69c6dd8eda3f131158e44b7
Edmark <edmark.aldea@norimaconsulting.com>
2022-04-25 10:54:40 +0800

    Added new string

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,5 +3,6 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello again World!");
+        System.out.println("I was here - Edmark");
     }
 }
