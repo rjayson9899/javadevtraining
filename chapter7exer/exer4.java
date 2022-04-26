db901395a66163a0922cb10ca221f5dd987aa08f
mac <mac.isaac@Norimaconsulting.com>
2022-04-26 16:06:40 +0800

    last work

---
+++ chapter7exer/exer4.java
@@ -0,0 +1,16 @@
+package chapter7exer;
+
+public class exer4 {
+    public void drawBar(int input){
+        for(int x = 0; x < input; x++){
+            for(int y = 0; y < input; y++){
+
+            }
+        }
+
+    }
+
+    public void addSpaces(){
+        System.out.print(" ");
+    }
+}
