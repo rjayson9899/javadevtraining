fd92829414ba6b977ff185750c6b7ff21137f2d0
mac <mac.isaac@Norimaconsulting.com>
2022-04-26 08:40:59 +0800

    trying to revert

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,11 +3,14 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello again World!");
+<<<<<<< HEAD
         System.out.println("I was here - Edmark");
         System.out.println("Mac was here <3");
         System.out.println("Ikam was here");
 
         System.out.println("For file conflict purposes.");
 
+=======
+>>>>>>> parent of 7296689 (Added new string)
     }
 }
