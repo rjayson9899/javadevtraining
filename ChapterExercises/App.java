b61be485647dbb0ae4cfa8ea8be0e8f636817242
Yks <macario.peralta@norimaconsulting.com>
2022-04-27 09:31:28 +0800

    added Loops.java

--- ChapterExercises/App.java
+++ ChapterExercises/App.java
@@ -2,8 +2,6 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-      Random rand = new Random();
-        int num1, num2, num3;
         System.out.format("%8s %8s %8s %8s %8s", "B", "I", "N", "G", "O\n");
         System.out.format("%8s %8s %8s %8s %8s", "2", "20", "42", "60", "64\n");
         System.out.format("%8s %8s %8s %8s %8s", "14", "25", "32", "55", "70\n");
---
+++ ChapterExercises/Loops.java
@@ -0,0 +1,12 @@
+public class Loops{
+
+    public static void runExOne(){
+      for (int x = 0; x < 10; x++){
+          System.out.println(x);
+      }  
+    }
+
+    public static void main(String[] args)throws Exception{
+     runExOne();
+    }
+}
