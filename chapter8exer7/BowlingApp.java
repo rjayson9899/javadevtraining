ddc998e5ab0e431d0587f849184bf55098b736e4
mac <mac.isaac@Norimaconsulting.com>
2022-04-28 12:03:53 +0800

    Finished chapter 8 exer 7

--- chapter8exer7/BowlingApp.java
+++ chapter8exer7/BowlingApp.java
@@ -22,16 +22,20 @@ public class BowlingApp {
             players.add(new BowlGame(inputName));
             players.get(x).setScore(0);
         }
+        System.out.println("\033[H\033[2J");
 
         for(int x = 0; x < 10; x++){
             
+            System.out.println("-------------");
             System.out.println("Frame: " + (x+1));
+            System.out.println("-------------");
 
             for(int y = 0; y < numPlayers; y++){
                 scoring = players.get(y).startFrame();
                 players.get(y).setScore(scoring);
                 System.out.println("score of " + players.get(y).getName() + " is: " + scoring);
-                System.out.println("Total score of " + players.get(y).getName() + " is: " + players.get(y).getScore());                
+                System.out.println("Total score of " + players.get(y).getName() + " is: " + players.get(y).getScore());  
+                System.out.println("");              
             }
             
         }
