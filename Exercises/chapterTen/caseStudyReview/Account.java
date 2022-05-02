ced93826e466dca1bc47c88f01bb78907e806eee
Roger <roger.mendez@norimaconsulting.com>
2022-05-02 14:34:11 +0800

    Initial Commit for Chapter 10 case study review

---
+++ Exercises/chapterTen/caseStudyReview/Account.java
@@ -0,0 +1,160 @@
+package Exercises.chapterTen.caseStudyReview;
+
+/**
+* Account class.
+*/
+import java.text.NumberFormat;
+
+public class Account {
+    private double balance;
+    private Customer cust;
+    private String acctID;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+    /**
+    * constructor
+    * pre: none
+    * post: An account has been created. Balance and
+    * customer data has been initialized with parameters.
+    */
+    public Account(double bal, String fName, String lName, String street, String city, String state, String zip) {
+        balance = bal;
+        cust = new Customer(fName, lName);
+        acctID = fName.substring(0,1) + lName;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    /**
+    * constructor
+    * pre: none
+    * post: An empty account has been created with the specified account ID.
+    */
+    public Account(String ID) {
+        balance = 0;
+        cust = new Customer("", "");
+        acctID = ID;
+    }
+
+    /**
+    * Returns the account ID.
+    * pre: none
+    * post: The account ID has been returned.
+    */
+    public String getID() {
+        return(acctID);
+    }
+
+    /**
+    * Returns the current balance.
+    * pre: none
+    * post: The account balance has been returned.
+    */
+    public double getBalance() {
+        return(balance);
+    }
+
+    /**
+    * A deposit is made to the account.
+    * pre: none
+    * post: The balance has been increased by the amount of the deposit.
+    */
+    public void deposit(double amt) {
+        balance += amt;
+    }
+
+    /**
+    * A withdrawal is made from the account if there is enough money.
+    * pre: none
+    * post: The balance has been decreased by the amount withdrawn.
+    */
+    public void withdrawal(double amt) {
+        if (amt <= balance) {
+            balance -= amt;
+        } else {
+            System.out.println("Not enough money in account.");
+        }
+    }
+
+    /**
+    * Change the street
+    * pre: none
+    * post: Street changed
+    */
+    public void changeStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+    * Change the city
+    * pre: none
+    * post: City changed
+    */
+    public void changeCity(String city) {
+        this.city = city;
+    }
+
+    /**
+    * Change the state
+    * pre: none
+    * post: State changed
+    */
+    public void changeState(String state) {
+        this.state = state;
+    }
+
+    /**
+    * Change the zip
+    * pre: none
+    * post: zip changed
+    */
+    public void changeZip(String zip) {
+        this.zip = zip;
+    }
+
+    /**
+    * Change the street, city, state, and zip
+    * pre: none
+    * post: Street, city, state, and zip changed
+    */
+    public void changeAddress(String street, String city, String state, String zip) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    /**
+    * Returns a true when objects have matching account IDs.
+    * pre: none
+    * post: true has been returned when the objects are equal,
+    * false returned otherwise.
+    */
+    public boolean equals(Object acct) {
+        Account testAcct = (Account)acct;
+        if (acctID.equals(testAcct.acctID)) {
+            return(true);
+        } else {
+            return(false);
+        }
+    }
+
+    /**
+    * Returns a String that represents the Account object.
+    * pre: none
+    * post: A string representing the Account object has been returned.
+    */
+    public String toString() {
+        String accountString;
+        NumberFormat money = NumberFormat.getCurrencyInstance();
+        accountString = acctID + "\n";
+        accountString += cust.toString();
+        accountString += "Current balance is " + money.format(balance);
+        return(accountString);
+    }
+}
---
+++ Exercises/chapterTen/caseStudyReview/LocalBank.java
@@ -0,0 +1,57 @@
+package Exercises.chapterTen.caseStudyReview;
+
+/**
+* A bank where accounts can be opened or closed and customers can
+* make transactions.
+*/
+import java.util.Scanner;
+
+public class LocalBank {
+    public static void main(String[] args) {
+        Bank easySave = new Bank();
+        Scanner input = new Scanner(System.in);
+        String action, acctID;
+        Double amt;
+
+        /* display menu of choices */
+        do {
+            System.out.println("\nDeposit\\Withdrawal\\Check balance");
+            System.out.println("Add an account\\Remove an account\\Modify and Account");
+            System.out.println("Quit\n");
+            System.out.print("Enter choice: ");
+            action = input.nextLine();
+            System.out.println(action);
+
+            if (action.equalsIgnoreCase("A")) {
+                easySave.addAccount(input);
+            } 
+            else if (!action.equalsIgnoreCase("Q")) {
+                System.out.print("Enter account ID: ");
+                acctID = input.nextLine();
+
+                if (action.equalsIgnoreCase("D")) {
+                    System.out.print("Enter deposit amount: ");
+                    amt = input.nextDouble();
+                    input.nextLine();
+                    easySave.transaction(1, acctID, amt);
+                } 
+                else if (action.equalsIgnoreCase("W")) {
+                    System.out.print("Enter withdrawal amount: ");
+                    amt = input.nextDouble();
+                    input.nextLine();
+                    easySave.transaction(2, acctID, amt);
+                } 
+                else if (action.equalsIgnoreCase("C")) {
+                    easySave.checkBalance(acctID);
+                } 
+                else if (action.equalsIgnoreCase("R")) {
+                    easySave.deleteAccount(acctID);
+                }
+                else if (action.equalsIgnoreCase("M")) {
+                    easySave.modifyAccount(acctID, input);
+                }
+            }
+        } while (!action.equalsIgnoreCase("Q"));
+        input.close();
+    }
+}
